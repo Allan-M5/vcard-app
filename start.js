b181901dd037e42eee01a1296640b4f12791501c
Allan Mujera <allanmujera91@gmail.com>
2025-06-18 19:46:16 +0300

    ✅ FIXED: Render deploy now uses dynamic PORT with start.js

---
+++ start.js
@@ -0,0 +1,14 @@
+﻿const { exec } = require("child_process");
+
+const port = process.env.PORT || 3000;
+exec(
+px serve dist --listen 0.0.0.0:, (err, stdout, stderr) => {
+  if (err) {
+    console.error(Error starting server: );
+    return;
+  }
+  if (stderr) {
+    console.error(stderr);
+  }
+  console.log(stdout);
+});
